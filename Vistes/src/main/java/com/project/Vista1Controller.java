42e413193ce5ff301879fe2f603e28564f538086
Victor Prieto <victorpg182006@gmail.com>
2025-09-26 16:51:47 +0200

    .

--- Vistes/src/main/java/com/project/Vista1Controller.java
+++ Vistes/src/main/java/com/project/Vista1Controller.java
@@ -1,53 +1,25 @@
 package com.project;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class Vista1Controller implements Initializable {
-    
+public class Vista1Controller {
+
     @FXML
     private TextField nomField;
-    
+
     @FXML
     private TextField edatField;
-    
+
     @FXML
-    private Button switchButton;
-    
-    private Main mainApp;
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // Desactiva inicialment el botó
-        switchButton.setDisable(true);
-        
-        // Escolta els canvis als camps de text per activar/desactivar el botó
-        nomField.textProperty().addListener((obs, oldVal, newVal) -> validateFields());
-        edatField.textProperty().addListener((obs, oldVal, newVal) -> validateFields());
-    }
-    
-    private void validateFields() {
-        switchButton.setDisable(nomField.getText().trim().isEmpty() || edatField.getText().trim().isEmpty());
-    }
-    
-    // Cridat quan es clica el botó
-    @FXML
-    private void handleSwitch() {
-        // Actualitza les variables estàtiques
-        Main.nom = nomField.getText().trim();
-        Main.edat = edatField.getText().trim();
-        
-        // Canvia a la segona vista
-        mainApp.switchToVista2();
-    }
-    
-    // Setter per la referència a Main (injectat via carregador FXML si cal)
-    public void setMainApp(Main mainApp) {
-        this.mainApp = mainApp;
+    public void canviaVista2() throws Exception {
+        String nom = nomField.getText();
+        String edat = edatField.getText();
+
+        if (!nom.isEmpty() && !edat.isEmpty()) {
+            Main.nom = nom;
+            Main.edat = edat;
+            Main.canviaVista("Vista2.fxml");
+        }
     }
 }
--- Vistes/src/main/java/com/project/Vista2Controller.java
+++ Vistes/src/main/java/com/project/Vista2Controller.java
@@ -1,37 +1,20 @@
 package com.project;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class Vista2Controller implements Initializable {
-    
+public class Vista2Controller {
+
     @FXML
-    private Label greetingLabel;
-    
+    private Label missatgeLabel;
+
     @FXML
-    private Button backButton;
-    
-    private Main mainApp;
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // Mostra la salutació utilitzant les variables estàtiques
-        greetingLabel.setText("Hola " + Main.nom + ", tens " + Main.edat + " anys!");
+    public void initialize() {
+        missatgeLabel.setText("Hola " + Main.nom + ", tens " + Main.edat + " anys!");
     }
-    
-    // Cridat quan es clica el botó de tornada
+
     @FXML
-    private void handleBack() {
-        mainApp.switchToVista1();
-    }
-    
-    // Setter per la referència a Main
-    public void setMainApp(Main mainApp) {
-        this.mainApp = mainApp;
+    public void tornaVista1() throws Exception {
+        Main.canviaVista("Vista1.fxml");
     }
 }
