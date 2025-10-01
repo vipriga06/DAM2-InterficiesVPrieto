c7e9f2cd538fbb72db21c3dd83e237d1ce9310f5
Victor Prieto <victorpg182006@gmail.com>
2025-10-01 19:12:40 +0200

    fix

--- Sega DB/src/main/java/com/segadb/ControllerMain.java
+++ Sega DB/src/main/java/com/segadb/ControllerMain.java
@@ -1,40 +1,94 @@
 package com.segadb;
 
-import com.utils.UtilsViews;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class ControllerMain {
+
+    @FXML
+    private VBox list1;
 
     @FXML
     private ImageView image;
 
     @FXML
-    private ImageView characterImage;
+    private Text nom;
 
+    @FXML
+    private Text nom1;
 
-
+    private List<CharacterData> characters = new ArrayList<>();
 
     @FXML
     public void initialize() {
-        Image img = new Image(getClass().getResourceAsStream("/icons/sega_icon.gif"));
-        image.setImage(img);
-        Image img_character = new Image(getClass().getResourceAsStream("/icons/characters-sega.gif"));
-        characterImage.setImage(img_character);
-        
+        loadCharactersFromJson();
+        System.out.println("Personajes cargados: " + characters.size());
+
+        try {
+            for (CharacterData character : characters) {
+                System.out.println("Cargando personaje: " + character.getName());
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/assets/subviewCharacters.fxml"));
+                AnchorPane item = loader.load(); // Cambiar VBox por AnchorPane aquí
+
+                ControllerItem1 controllerItem = loader.getController();
+                controllerItem.setTitle(character.getName());
+                controllerItem.setSubtitle(character.getGame());
+                controllerItem.setImage("/assets/imagesTot/" + character.getImage());
+                controllerItem.setCircleColor(character.getColor());
+
+                item.setOnMouseClicked(event -> {
+                    System.out.println("Seleccionado: " + character.getName());
+                    image.setImage(new Image(getClass().getResourceAsStream("/assets/imagesTot/" + character.getImage())));
+                    nom.setText(character.getName());
+                    nom1.setText(character.getGame());
+                });
+
+                list1.getChildren().add(item);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    @FXML
-    private void toViewCharacters(MouseEvent event) {
-        System.out.println("To View Characters");
-        ControllerCharacters ctrlCharacters = (ControllerCharacters) UtilsViews.getController("ViewCharacters");
-        ctrlCharacters.loadList();
-        UtilsViews.setViewAnimating("ViewCharacters");
+    private void loadCharactersFromJson() {
+        try (InputStream is = getClass().getResourceAsStream("/assets/data/characters_sega.json")) {
+            if (is == null) {
+                System.err.println("No se encontró el archivo JSON");
+                return;
+            }
+
+            String jsonText;
+            try (Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.name())) {
+                jsonText = scanner.useDelimiter("\\A").next();
+            }
+
+            JSONArray array = new JSONArray(jsonText);
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject obj = array.getJSONObject(i);
+                String name = obj.getString("name");
+                String image = obj.getString("image");
+                String color = obj.getString("color");
+                String game = obj.getString("game");
+
+                characters.add(new CharacterData(name, image, color, game));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-
-
 }
--- Sega DB/src/main/java/com/segadb/Main.java
+++ Sega DB/src/main/java/com/segadb/Main.java
@@ -1,71 +1,25 @@
 package com.segadb;
 
-import com.utils.UtilsViews;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-// Fes anar l'exemple amb:
-// ./run.sh com.exercici0601.Main
 
 public class Main extends Application {
 
-    final int WINDOW_WIDTH = 400;
-    final int WINDOW_HEIGHT = 500;
-    final int MIN_WIDTH = 400;
-    final int MIN_HEIGHT = 500;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carga el archivo FXML principal
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/assets/viewMain.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Sega DB");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-
-        UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
-        UtilsViews.addView(getClass(), "ViewMain", "/assets/viewMain.fxml");
-        UtilsViews.addView(getClass(), "ViewCharacters", "/assets/viewCharacters.fxml");
-        UtilsViews.addView(getClass(), "ViewCharacter", "/assets/viewCharacter.fxml");
-
-        Scene scene = new Scene(UtilsViews.parentContainer);
-
-
-        stage.setScene(scene);
-        stage.setTitle("Nintendo DB");
-        stage.setMinWidth(MIN_WIDTH);
-        stage.setWidth(WINDOW_WIDTH);
-        stage.setMinHeight(MIN_HEIGHT);
-        stage.setHeight(WINDOW_HEIGHT);
-        stage.show();
-
-
-        // Afegeix un listener per detectar canvis en les dimensions de la finestra
-        stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            System.out.println("Width changed: " + newVal);
-        });
-
-        stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            System.out.println("Height changed: " + newVal);
-        });
-
-
-        // Afegeix una icona només si no és un Mac
-        if (!System.getProperty("os.name").contains("Mac")) {
-            Image icon = new Image("file:icons/icon.png");
-            stage.getIcons().add(icon);
-        }
-
-
-        
-    }
-
-    private void _setLayout(int width) {
-        if (width < 600) {
-            UtilsViews.setView("Mobile");
-        } else {
-            UtilsViews.setView("Desktop");
-        }
     }
 }
